0358db9f854ba29df8771f80f48c635653b23bf3
Alfons Gonzalez Pauner <alfons.gonzalez.pauner@gmail.com>
2018-02-08 14:27:59 +0100

    Add files via upload

--- check-gluster-volums.sh
+++ check-gluster-volums.sh
@@ -3,15 +3,30 @@
 # script per fer un check dels volums del gluster
 #
 LLISTA=`gluster volume info | grep Volume | grep Name | awk '{ print $3}'`
+# fitxer de control
+# el creem i ens assegurem que esta buit
+if [ ! /tmp/volums.txt ]
+then
+        touch /tmp/volums.txt
+fi
+cat /dev/null > /tmp/volums.txt
+# Mirem com estan 
 for VOL in $LLISTA
 	do
 	   ESTAT=`gluster volume info $VOL | grep Status | awk '{print $2}'`
-	   if [ "$ESTAT" == "Started" ]
-	   then
-		echo "$VOL OK"
-	   else
-		echo "$VOL No OK"
-	   fi
-	done
+	   echo $VOL $ESTAT >> /tmp/volums.txt
+        done
+DOLENT=`cat /tmp/volums.txt | grep -v Started`
+ESTAT_FINAL=`cat /tmp/volums.txt`
+if [ -z "$DOLENT" ]
+then
+        echo "$ESTAT_FINAL"
+        exit 0
+fi
+if [ -n "$RESULT" ]
+then
+        echo "CRITICAL: $ESTAT_FINAL"
+        exit 2
+fi
 
 
