7f4a728bc001c74e277e2c2389ab2d19b645f401
Alfons Gonzalez Pauner <alfons.gonzalez.pauner@gmail.com>
2018-05-02 16:57:19 +0200

    Add files via upload

--- desmuntat-sshfs.sh
+++ desmuntat-sshfs.sh
@@ -9,10 +9,10 @@ for muntatge in $remotes
 	proces=`ps aux | grep $muntatge | grep -v grep| awk {'print $2'}`
 	usuari=`ps aux | grep $muntatge | grep -v grep| awk {'print $1'}`
 	puntdemuntatge=`grep $muntatge /etc/mtab | awk {'print $2'}`
-	control=`df -h | grep $puntdemuntatge | grep -c "Transport endpoint is not connected" `
+	control=`su - $usuari -c "df -h | grep -c $puntdemuntatge"`
 	echo $control
 	# desmuntem el filesystem si encara esta al mtab pero ja no esta muntat
-	if [ $control -gt 0 ]; then
+	if [ $control -lt 1 ]; then
 		su - $usuari -c "fusermount -u $puntdemuntatge"
 	fi
 	done
